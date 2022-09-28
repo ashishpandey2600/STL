bf47ecefaf5d471399f9e5d65f33d55029ec8749
ashishpandey2600 <ashishpandey2600@gmail.com>
2022-09-28 11:25:28 +0530

    Multimap

---
+++ Multimap.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n;
+	cin>>n;
+
+	map<int , string> m;
+
+	for(int i=0;i<n;++i)
+	{
+		int x;
+		string y;
+		cin>>x>>y;
+		m.insert({x,y});
+	}
+	
+}
